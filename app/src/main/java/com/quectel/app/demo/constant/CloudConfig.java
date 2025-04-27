98dbac511e4bf4045f8f6175a5718f1c001f1150
邓元鹏 <tommy.deng@quectel.com>
2025-04-27 18:59:54 +0800

    增加区域说明

--- app/src/main/java/com/quectel/app/demo/constant/CloudConfig.java
+++ app/src/main/java/com/quectel/app/demo/constant/CloudConfig.java
@@ -9,12 +9,15 @@ public class CloudConfig {
     public static final String QUEC_DOMAIN_SECRET = "quecDomainSecret";
     public static final String QUEC_ClOUD_SERVICE_TYPE = "QuecCloudServiceType";
 
+    //中国区域
     public static final String DATA_CENTER_CHINA_USER_DOMAIN = "";
     public static final String DATA_CENTER_CHINA_DOMAIN_SECRET = "";
 
+    //欧洲区域
     public static final String DATA_CENTER_EUROPE_USER_DOMAIN = "";
     public static final String DATA_CENTER_EUROPE_DOMAIN_SECRET = "";
 
+    //北美区域
     public static final String DATA_CENTER_AMERICA_USER_DOMAIN = "";
     public static final String DATA_CENTER_AMERICA_DOMAIN_SECRET = "";
 
