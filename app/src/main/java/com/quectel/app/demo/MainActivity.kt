3ca75830227940de3ffac588624872cce5cddb41
邓元鹏 <tommy.deng@quectel.com>
2025-04-30 10:50:20 +0800

    token处理逻辑完善

--- app/src/main/java/com/quectel/app/demo/MainActivity.kt
+++ app/src/main/java/com/quectel/app/demo/MainActivity.kt
@@ -1,30 +1,33 @@
-package com.quectel.app.demo;
+package com.quectel.app.demo
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.quectel.app.demo.ui.HomeActivity
+import com.quectel.app.demo.ui.StartActivity
+import com.quectel.app.usersdk.service.QuecUserService
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-import com.quectel.app.demo.ui.HomeActivity;
-import com.quectel.app.demo.ui.StartActivity;
-import com.quectel.app.usersdk.service.QuecUserService;
+        QuecUserService.checkUserLoginState {
+            if (it.isSuccess && it.data) {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this@MainActivity, StartActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (QuecUserService.INSTANCE.isLogin()) {
-            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            Intent intent = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(intent);
-            finish();
+        QuecUserService.setTokenInvalidCallBack {
+            val intent = Intent(this@MainActivity, StartActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
-
 }
